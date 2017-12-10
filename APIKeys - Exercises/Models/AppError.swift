d69e40ef1a5ebd0ed99d2d62a822e9258b00f0bd
Marlon Rugama <MarlonRugama@ac.c4q.nyc>
2017-12-09 20:47:43 -0500

    added a comment

--- APIKeys - Exercises/Models/AppError.swift
+++ APIKeys - Exercises/Models/AppError.swift
@@ -7,15 +7,18 @@
 //
 
 import Foundation
-//enum AppError: Error {
-//    case unauthenticated
-//    case invalidJSONResponse
-//    case couldNotParseJSON(rawError: Error)
-//    case noInternetConnection
-//    case badURL
-//    case badStatusCode
-//    case noDataReceived
-//    case notAnImage
-//    case other(rawError: Error)
-//}
+enum AppError: Error {
+    case unauthenticated
+    case invalidJSONResponse
+    case couldNotParseJSON(rawError: Error)
+    case noInternetConnection
+    case badURL
+    case badStatusCode
+    case noDataReceived
+    case notAnImage
+    case other(rawError: Error)
+}
+
+//This is a comment
+
 
